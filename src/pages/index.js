b2f31a85bea2ffcd2fc156d1af41adf0c615111e
molimolinari <59256322+molimolinari@users.noreply.github.com>
2024-12-17 18:37:58 -0300

    Update src/pages/index.js (via Pages CMS)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,3 @@
-// Step 1: Import React
-import * as React from 'react'
-import Layout from '../components/layout'
-import { StaticImage } from 'gatsby-plugin-image'
-
-// Step 2: Define your component
-const IndexPage = () => {
-  return (
-    <main>
-    <Layout pageTitle="Home Page">
-      <p>El Perrito</p>
-      <StaticImage
-        alt="El perrito"
-        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
-      />
-      <p>Y el mate</p>
-      <StaticImage
-        alt="Lenguaje del mate"
-        src="../images/mate.png"
-      />
-    </Layout>
-    </main>
-  )
-}
-
-// You'll learn about this in the next task, just copy it for now
-export const Head = () => <title>Home Page</title>
-
-// Step 3: Export your component
-export default IndexPage
+---
+title: Me vuelve loco esto
+---
